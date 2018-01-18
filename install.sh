b4e3e1a8f6ddf3b353a55cf0541dc0a5f88cd220
hcl <hcltho@gmail.com>
2018-01-18 19:12:16 +0800

    Fix bugs.

--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ mkdir -p /etc/iptables
 cp ./rules.v* /etc/iptables
 cp ./iptables /etc/network/if-pre-up.d
 
+iptables-restore < /etc/iptables/rules.v4
+ip6tables-restore < /etc/iptables/rules.v4
+
 iptables -nvL --line-number
 ip6tables -nvL --line-number
