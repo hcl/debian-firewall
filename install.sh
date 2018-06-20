8f2f3132ada736864b81a951d1c796677ee293fd
hcl <hcltho@gmail.com>
2018-06-20 20:39:49 +0800

    Fix a typo

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cp ./rules.v* /etc/iptables
 cp ./iptables /etc/network/if-pre-up.d
 
 iptables-restore < /etc/iptables/rules.v4
-ip6tables-restore < /etc/iptables/rules.v4
+ip6tables-restore < /etc/iptables/rules.v6
 
 iptables -nvL --line-number
 ip6tables -nvL --line-number
